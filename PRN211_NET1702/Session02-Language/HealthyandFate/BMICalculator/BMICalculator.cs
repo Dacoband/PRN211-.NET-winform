6ee2b17ca85890bb2c822565d16597aba71476fc
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-01-10 17:46:31 +0700

    UDPATE
    UPDATE

---
+++ PRN211_NET1702/Session02-Language/HealthyandFate/BMICalculator/BMICalculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bmi 
+{
+    internal class BMICalculator
+    {
+        public static double GetBMI(double weight, double height) => weight / (height * height);
+    }
+}
