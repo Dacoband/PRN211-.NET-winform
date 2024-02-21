e479d90ee403df533c25bdaec83e148286f64b63
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-02-21 13:08:49 +0700

    update Session4

---
+++ PRN211_NET1702/Session04-Collection/YearEndSchoolManager/Repositories/Entities/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Entities
+{
+    public class Student
+    {//put Property here, ToString()
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Yob { get; set; }
+        public double Gpa { get; set; }
+
+        public override string ToString() => $"Student: {Id} | {Name} | {Email} | {Yob} | {Gpa}";
+    }
+}
