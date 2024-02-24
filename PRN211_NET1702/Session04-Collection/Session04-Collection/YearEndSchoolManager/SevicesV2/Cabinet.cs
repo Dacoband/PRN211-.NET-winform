836eefa66b5d3a10b0c80a8985b53d3dea434a05
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-02-24 15:20:52 +0700

    Alter Folder

---
+++ PRN211_NET1702/Session04-Collection/Session04-Collection/YearEndSchoolManager/SevicesV2/Cabinet.cs
@@ -0,0 +1,25 @@
+﻿namespace SevicesV2
+{
+    public class Cabinet<T>
+
+    {
+        // a list of concreate variable or an array
+        //OR A LIST(LIKE IN JAVA) - NO LONGER _COUNT
+         //T: Type, can pass a data tuupe
+
+        //CRUD METHODS NORMAL
+        List <T> list = new List <T> ();
+
+        //Aray Líst <Student> _list = new Arraylist 
+              
+
+        //không cần biến count vì giỏ co giản như túi doraemon 
+        //đc quyền for đến hết vì mảng cấp theo nh ucaauf thực sự dùng 
+        //bên java .Size() trả về số phần tử đg có 
+        //bên c#  .Count{get;} giống .Length của mảng 
+
+
+
+
+    }
+}
