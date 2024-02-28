37d467f568f53573e34326560be60574d1a13f2d
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-02-28 22:28:17 +0700

    Add Delegate

--- PRN211_NET1702/DelegateInsideOut/DelegateReview.LambdaExpressV2/obj/Debug/net6.0/DelegateReview.LambdaExpressV2.AssemblyInfo.cs
+++ PRN211_NET1702/DelegateInsideOut/DelegateReview.LambdaExpressV2/obj/Debug/net6.0/DelegateReview.LambdaExpressV2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DelegateReview.LambdaExpressV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14b22b28eddbad4373b37dbc01ee551442b115a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DelegateReview.LambdaExpressV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DelegateReview.LambdaExpressV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
