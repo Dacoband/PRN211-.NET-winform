789383990ee716a85780a5c3ea1ce7142963ab8c
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-03-02 16:08:46 +0700

    1

--- PRN211_NET1702/DelegateInsideOut/DelegateReview.LamdaExpressionV3/obj/Debug/net6.0/DelegateReview.LamdaExpressionV3.AssemblyInfo.cs
+++ PRN211_NET1702/DelegateInsideOut/DelegateReview.LamdaExpressionV3/obj/Debug/net6.0/DelegateReview.LamdaExpressionV3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DelegateReview.LamdaExpressionV3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14b22b28eddbad4373b37dbc01ee551442b115a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97de4c69b9acd1cadc5db6a3299d50975bd91d23")]
 [assembly: System.Reflection.AssemblyProductAttribute("DelegateReview.LamdaExpressionV3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DelegateReview.LamdaExpressionV3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
